30e61185b3bf4d8636fa6118ee6dadd8ceb10390
John Paul Dugaojo <johnpauldugaojo@gmail.com>
2020-08-27 22:25:07 +0800

    Ready to Deploy

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About this App</h1>
+      <p>Web Application to search Github Users using React JS </p>
+      <p>Version: 1.0</p>
+    </Fragment>
+  );
+};
+
+export default About;
---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1 style={{ color: 'gray', textAlign: 'center' }}>Not Found❌</h1>
+      <p className='lead' style={{ textAlign: 'center' }}>
+        {' '}
+        The page that you are looking for is not exist!!!
+      </p>
+    </div>
+  );
+};
+
+export default NotFound;
